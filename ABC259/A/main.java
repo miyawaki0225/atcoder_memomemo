96f1930cfff9ad248c7d1a79cafb5a2fbc7f5e0f
miyawaki0225 <m-miyawaki@rapide-act.com>
2022-09-01 16:55:14 +0900

    【Add】ABC259
    ABC259開催

---
+++ ABC259/A/main.java
@@ -0,0 +1,11 @@
+package ABC259.A;
+
+import java.util.Scanner;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World!!");
+        sc.close();
+    }
+}
