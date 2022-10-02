871b12fbce62c64fa955d4d620c11c5492b0c219
m-miyawaki <m-miyawaki@rapide-act.com>
2022-10-03 00:34:58 +0900

    【Fix】ABC271_C

--- ABC271/C/Main.java
+++ ABC271/C/Main.java
@@ -1,42 +1,41 @@
 package ABC271.C;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+ 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] a_array = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        Arrays.sort(a_array);
-        int[] result_array = Arrays.copyOf(a_array, a_array.length);
-        int array_position=0;
-        int count = 0;
-        if(n > 1){
-            for(int i=0;;){
-                if(a_array[array_position] == (i+1)){
-                    count++;
-                    i++;
-                    array_position++;
-                } else {
-                    n -= 2;
-                    if(n > 0){
-                        result_array[n]=0;
-                        result_array[n+1]=0;
-                        if( result_array[array_position]!=0){
-                            count++;
-                            i++;
-                        } else {
-                            break;
-                        }
-                        continue;
-                    } else {
-                        break;
-                    }
-                }
+    public static void main(String[] args) {
+        // 不正解中 10/03
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        
+        List<Integer> n_list = Arrays.stream(sc.nextLine().split(" "))
+        .map(Integer::parseInt)
+        .collect(Collectors.toList());
+        Collections.sort(n_list);
+        LinkedList<Integer> list = new LinkedList<>(n_list);
+        
+        while (list.size() != n) list.add(-1);
+ 
+        for (int i = 1; i <= n; i++) {
+            if (list.getFirst() == i) {
+                list.removeFirst();
+            } else if (list.size() < 2) {
+                System.out.println(i-1);
+                break;
+            } else {
+                list.removeLast();
+                list.removeLast();
+            }
+            if (list.size() == 0) {
+                System.out.println(i);
+                break;
             }
         }
-        System.out.println(count);
+        sc.close();
     }
 }
